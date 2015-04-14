d38de9abdda2f37f74237cd7df37d49c3ba98f4f
marcelonsimoni <mnunessi@hotmail.com>
2015-04-13 21:20:11 -0300

    Atualização do texto

--- exercicio4/index.php
+++ exercicio4/index.php
@@ -18,7 +18,7 @@
         
         	<div id="texto-topo">
             <strong>Exercício número 4</strong><br />Desenvolva uma API Rest para um sistema gerenciador de tarefas
-(inclusão/alteração/exclusão). As tarefas consistem em título e descrição, ordenadas por
+(inclusão/alteração/exclusão).<br />As tarefas consistem em título e descrição, ordenadas por
 prioridade.
             </div>
             <div id="estrutura-menu">
